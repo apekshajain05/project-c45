039932a9b0b7120b9dbbcea4fb92e0d5c0af3cdb
apekshajain05 <62800258+apekshajain05@users.noreply.github.com>
2020-07-14 01:05:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var form;
+var taskName;
+var input,button,title;
+var ToDo;
+
+
+function setup() {
+  createCanvas(20,20);
+  database=firebase.database(); 
+  title=createElement("h1");
+  title.html("TO DO LIST");
+  title.position(430,30);
+   input=createInput("Enter a Task");
+   input.position(470,150);
+   button=createButton("Add Task");
+   button.position(650,150);
+   button.mousePressed(submitData);
+}
+
+function draw() {
+  background(255,255,255);  
+ 
+}
+
+function submitData(){
+  var data={
+    taskName: input.value()
+  }
+  var db=database.ref("ToDo");
+  db.push(data);
+  //console.log(data);
+}
